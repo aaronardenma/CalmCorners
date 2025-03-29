82751a167c9271ff36e4fe5373d9b9a1880ef540
Justin Galimpin <jgalimpi@student.ubc.ca>
2025-03-29 15:11:10 -0700

    updates to server.ts to include location route

--- backend/database/server.ts
+++ backend/database/server.ts
@@ -4,10 +4,11 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import locationRoutes from "./routes/Locations";
 import reviewRoutes from "./routes/Reviews";
 
 dotenv.config({ path: "db.env" });
+
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Default to 5000 if not specified
@@ -15,6 +16,10 @@ const mongoURI = process.env.DB_URI as string;
 
 app.use(express.json()); // Parse JSON request body
 app.use(cors()); // Allow cross-origin requests
+
+app.use("/api/locations", locationRoutes);
+app.use("/api/reviews", reviewRoutes);
+
 
 // Connect to MongoDB
 mongoose.connect(mongoURI)
@@ -25,10 +30,10 @@ mongoose.connect(mongoURI)
         console.error("MongoDB connection error:", error);
     });
 
-    app.get("/", (req, res) => {
-        res.send("Quiet Space Finder API is running...");
-    });
-    
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+app.get("/", (req, res) => {
+    res.send("CalmCorners API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
