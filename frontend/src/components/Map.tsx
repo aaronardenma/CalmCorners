0368ff51b033b2a2acd68241834c50e47bb631b8
Janie H <janieholzmann@gmail.com>
2025-03-30 10:17:21 -0700

    fetch hardcoded locations for markers

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -24,6 +24,96 @@ const center = {
 
 const key = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY;
 
+async function searchPlaces(text: Array<string>, map: any) {
+  //@ts-ignore
+  const { Place, SearchNearbyRankPreference } = await google.maps.importLibrary('places') as google.maps.PlacesLibrary;
+
+  const resp = text.map(async (txt: string) => {  
+    console.log(txt);
+    const request = {
+    textQuery: txt,
+    fields: ['displayName', 'location'],
+    locationBias: { lat: 49.266757915974424, lng: -123.25493253691926 },
+    maxResultCount: 8,
+    minRating: 1.0,
+  };
+  const place = await Place.searchByText(request);
+  console.log(place);
+  return place;
+
+  })
+
+  const places = await Promise.all(resp);
+  console.log("PLACES: ", places.length);
+
+      if (places.length) {
+        console.log(places);
+
+        const { LatLngBounds } = await google.maps.importLibrary("core") as google.maps.CoreLibrary;
+        const bounds = new LatLngBounds();
+
+        // Loop through and get all the results.
+        places.forEach((place: any) => {
+          console.log("Choosing colours...");
+          const loc = place.places[0].Eg.location;
+          const dispName = place.places[0].Eg.displayName;
+          console.log("location: ", loc);
+          console.log("Display name: ", dispName);
+          const col = chooseColour();
+          let noise;
+          let busy;
+          if (col == '#4CAF50') {
+            noise = "Quiet";
+            busy = "Not busy - lots of space";
+          } else if (col == '#FFC107') {
+            noise = "Moderate noise";
+            busy = "Somewhat busy";
+          } else {
+            noise = "Noisy";
+            busy = "Very busy - no more open spots";
+          }
+            const markerView = new google.maps.Marker({
+                map,
+                position: loc,
+                title: place.displayName,
+                icon: {
+                  path: window.google.maps.SymbolPath.CIRCLE,
+                  fillColor: col, 
+                  fillOpacity: 0.6,
+                  strokeWeight: 1,
+                  strokeColor: '#ffffff',
+                  scale: 10,
+      },
+            });
+    // Add an info window for additional details
+    const infoWindow = new google.maps.InfoWindow({
+      content: `
+        <div style='color: black;'>
+          <h2>${dispName}</h2>
+          <p>Noise Level: ${noise}</p>
+          <p>${busy}</p>
+        </div>
+      `,
+    });
+            markerView.addListener('click', () => {
+                  infoWindow.open({
+                    anchor: markerView,
+                    map,
+    });
+            });
+            bounds.extend(loc as google.maps.LatLng);
+            console.log(place);
+        });
+
+        map.fitBounds(bounds);
+
+    } else {
+        console.log("No results");
+    }
+
+
+}
+
 async function nearbySearch(map: any) {
     //@ts-ignore
     const { Place, SearchNearbyRankPreference } = await google.maps.importLibrary('places') as google.maps.PlacesLibrary;
@@ -71,7 +161,7 @@ async function nearbySearch(map: any) {
                 title: place.displayName,
                 icon: {
                   path: window.google.maps.SymbolPath.CIRCLE,
-                  fillColor: col,
+                  fillColor: col, 
                   fillOpacity: 0.6,
                   strokeWeight: 1,
                   strokeColor: '#ffffff',
@@ -350,10 +440,6 @@ const MapView: React.FC = () => {
   const mapRef = useRef<google.maps.Map | null>(null); // Reference to map object
   const [isMapLoaded, setIsMapLoaded] = useState(false); // State to track if map is loaded
 
-  // const { isLoaded } = useJsApiLoader({
-  //   googleMapsApiKey: key, // Your API Key
-  // });
-
   // Wait for the map to load
   const onMapLoad = (map: google.maps.Map) => {
     mapRef.current = map;  // Set the map reference once map is loaded
@@ -375,8 +461,10 @@ const MapView: React.FC = () => {
       marker.addListener('click', () => {
         alert('Marker clicked!');
       });
+      const places = ['Irving K. Barber Learning Centre', 'Koerner Library', 'AMS Student Nest', 'UBC Life Building'];
+      searchPlaces(places, map);
 
-      nearbySearch(map);
+      //nearbySearch(map);
     }
   }, [isMapLoaded]);  // This effect runs once when map is loaded
 
@@ -405,6 +493,10 @@ function chooseColour() {
 
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
+}
+
+function getReviewsColour(place: any) {
+
 }
 
 export default MapView;
